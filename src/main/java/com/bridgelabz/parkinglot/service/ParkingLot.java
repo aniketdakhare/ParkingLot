7dadeb94b17780fa53b0b0d2a614099af2b08980
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-17 23:00:56 +0530

    ADDED Code to add car number to a parking list in order to park the car.

--- src/main/java/com/bridgelabz/parkinglot/service/ParkingLot.java
+++ src/main/java/com/bridgelabz/parkinglot/service/ParkingLot.java
@@ -1,10 +1,15 @@
 package com.bridgelabz.parkinglot.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot
 {
+    private List<String> parkingList = new ArrayList<>();
 
     public boolean park( String carNumber)
     {
+        parkingList.add(carNumber);
         return true;
     }
 }
