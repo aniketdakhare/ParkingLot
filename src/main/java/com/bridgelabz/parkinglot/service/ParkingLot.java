5b5bdc346e2bb0b308ed725e045f4d4d0ea71c03
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-17 22:47:47 +0530

    ADDED Test case to get true value if the Car is parked by adding its detail.

---
+++ src/main/java/com/bridgelabz/parkinglot/service/ParkingLot.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.parkinglot.service;
+
+public class ParkingLot
+{
+
+    public boolean park( String carNumber)
+    {
+        return true;
+    }
+}
--- src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
@@ -1,5 +1,16 @@
 package com.bridgelabz.parkinglottest;
 
+import com.bridgelabz.parkinglot.service.ParkingLot;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ParkingLotTest
 {
+    @Test
+    public void givenCarDetails_WhenAddedToParkingLot_ShouldReturnTrue()
+    {
+        ParkingLot parkingLot = new ParkingLot();
+        boolean parked = parkingLot.park("MH-32-AW-4348");
+        Assert.assertTrue(parked);
+    }
 }
