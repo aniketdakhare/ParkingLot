a086a614a867d8aa26017564c03f265a4f06d34f
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-18 00:43:54 +0530

    ADDED Test case to get custom exception if the given car detail is incorrect.

--- src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglottest/ParkingLotTest.java
@@ -1,15 +1,24 @@
 package com.bridgelabz.parkinglottest;
 
+import com.bridgelabz.parkinglot.exception.ParkingLotException;
 import com.bridgelabz.parkinglot.service.ParkingLot;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest
 {
+    ParkingLot parkingLot;
+
+    @Before
+    public void setUp()
+    {
+        parkingLot = new ParkingLot();
+    }
+
     @Test
     public void givenCarDetails_WhenAddedToParkingLot_ShouldReturnTrue()
     {
-        ParkingLot parkingLot = new ParkingLot();
         boolean parked = parkingLot.park("MH-32-AW-4348");
         Assert.assertTrue(parked);
     }
@@ -17,9 +26,15 @@ public class ParkingLotTest
     @Test
     public void givenCarDetails_WhenProvidedToUnParkTheCar_ShouldReturnTrue()
     {
-        ParkingLot parkingLot = new ParkingLot();
         parkingLot.park("MH-32-AW-4348");
         boolean parked = parkingLot.unPark("MH-32-AW-4348");
         Assert.assertTrue(parked);
+    }
+
+    @Test
+    public void givenIncorrectCarDetails_WhenProvidedToUnParkTheCar_ShouldGiveException()
+    {
+        parkingLot.park("MH-32-AW-4348");
+        parkingLot.unPark("MH-34-AW-7555");
     }
 }
